303620855d7a90d7db526226d3aec0d93d44f9df
komal-kumbhar <komalkumbhar9898@gmail.com>
2024-10-24 14:24:11 +0530

    Project react-basic
    Project react-basic

---
+++ ReactB13Second/react-basic/src/App.js
@@ -0,0 +1,39 @@
+// import logo from './logo.svg';
+import './App.css';
+import Assignment from './Assignments/Assignment';
+// import FragComp from './JsxFragment/FragComp';
+// import AddNumInComp from './Assignments/AddNumInComp';
+// import CounterInComp from './Assignments/CounterInComp';
+// import JsxComponent from './JsxFragment/JsxComponent';
+//  import CodemindTutorial from './componentBasic/CodemindTutorial';
+// import JSFuncComp from './componentTypes/JSFuncCompo';
+// import JSClassComp from './componentTypes/JSClassComp';
+// import Welcome from './componentTypes/Welcome';
+// import WelcomeClass from './componentTypes/JSClassComp';
+//  import CodemindClassTutorial from './componentTypes/CodemindClassTutorial';
+// import CodemindTutorial from './componentBasic/CodemindTutorial';     
+// import CodemindMain from './componentBasic/CodemindMain';
+// import CodemindClassMain from './componentTypes/CodemindClassMain';
+
+function App() {
+  return (
+    <div className="App">
+    {/* <CodemindTutorial /> */}
+      {/* <CodemindMain/> */}
+      {/* <JSFuncComp/> */}
+      {/* <JSClassComp/> */}
+      {/* <Welcome/>
+      <WelcomeClass/> */}
+      {/* <CodemindClassTutorial/> */}
+      {/* <CodemindClassMain/> */}
+      {/* <CounterInComp/> */}
+      {/* <AddNumInComp/> */}
+      {/* <JsxComponent/> */}
+      {/* <FragComp/> */}
+      <Assignment/>
+      
+    </div>
+  );
+}
+
+export default App;
